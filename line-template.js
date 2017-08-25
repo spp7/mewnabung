ce2728f5f510bd45adcdbe64c58f716d05174fcd
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-08-25 13:14:49 +0700

    carousel edit

--- line-template.js
+++ line-template.js
@@ -2,55 +2,55 @@
 
 const carouselTemplate = function(messages) {
   return {
-    "type": "template",
-    "altText": "this is a carousel template",
-    "template": {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
       "type": "carousel",
       "columns": [
-        {
-          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-          "title": "this is menu",
-          "text": "description",
-          "actions": [
-            {
-                "type": "message",
-                "label": "Buy",
-                "data": "action=buy&itemid=111"
-            },
-            {
-                "type": "message",
-                "label": "Add to cart",
-                "data": "action=add&itemid=111"
-            },
-            {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/111"
-            }
-          ]
-        },
-        {
-          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-          "title": "this is menu",
-          "text": "description",
-          "actions": [
-            {
-                "type": "postback",
-                "label": "Buy",
-                "data": "action=buy&itemid=222"
-            },
-            {
-                "type": "postback",
-                "label": "Add to cart",
-                "data": "action=add&itemid=222"
-            },
-            {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/222"
-            }
-          ]
-        }
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
       ]
     }
   }
