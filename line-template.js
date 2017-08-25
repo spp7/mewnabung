f7dfc8287fe244f5311fc57ae8b188ed1f33abbf
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-08-25 13:32:51 +0700

    carousel edit

--- line-template.js
+++ line-template.js
@@ -6,7 +6,58 @@ const carouselTemplate = function(messages) {
   "altText": "this is a carousel template",
   "template": {
       "type": "carousel",
-      "columns": messages
+      "columns": [
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+          "title": "Games",
+          "text": "Mainkan games seru, dan dapatkan poin!",
+          "actions": [
+            {
+              "type": "postback",
+              "label": "Hangman",
+              "text": "Hangman",
+              "data": "Hangman"
+            },
+            {
+              "type": "postback",
+              "label": "Quiz BCA",
+              "text": "Quiz BCA",
+              "data": "Quiz BCA"
+            }
+          ]
+        },
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+          "title": "Akun Kamu",
+          "text": "Lihat detail akunmu",
+          "actions": [
+            {
+              "type": "postback",
+              "label": "Cek Saldo",
+              "text": "Cek Saldo",
+              "data": "Cek Saldo"
+            },
+            {
+              "type": "postback",
+              "label": "Alokasi Dana",
+              "text": "Alokasi Dana",
+              "data": "Alokasi Dana"
+            },
+            {
+              "type": "postback",
+              "label": "Set Goals",
+              "text": "Set Goals",
+              "data": "Set Goals"
+            },
+            {
+              "type": "postback",
+              "label": "Cek Poin",
+              "text": "Cek Poin",
+              "data": "Cek Poin"
+            }
+          ]
+        }
+      ]
     }
   }
 }
