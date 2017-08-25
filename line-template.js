f46355f2920de85e3d822c9aa0004cf23143bf6c
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-08-25 14:02:27 +0700

    carousel edit

--- line-template.js
+++ line-template.js
@@ -1,57 +1,12 @@
 'use strict';
 
-const carouselTemplate = function() {
+const carouselTemplate = function(messages) {
   return {
     "type": "template",
     "altText": "this is a carousel template",
     "template": {
-        "type": "carousel",
-        "columns": [
-            {
-              "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-              "title": "this is menu",
-              "text": "description",
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Buy",
-                      "data": "action=buy&itemid=111",
-                  },
-                  {
-                      "type": "message",
-                      "label": "Add to cart",
-                      "data": "action=add&itemid=111"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/111"
-                  }
-              ]
-            },
-            {
-              "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-              "title": "this is menu",
-              "text": "description",
-              "actions": [
-                  {
-                      "type": "postback",
-                      "label": "Buy",
-                      "data": "action=buy&itemid=222"
-                  },
-                  {
-                      "type": "postback",
-                      "label": "Add to cart",
-                      "data": "action=add&itemid=222"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/222"
-                  }
-              ]
-            }
-        ]
+      "type": "carousel",
+      "columns": messages
     }
   }
 }
@@ -82,17 +37,27 @@ const confirm = function(message) {
   }
 }
 
-const replyMessageFormat = function(type, messages='') {
+const replyMessageFormat = function(type, messages) {
   var replyFormat
   switch (type) {
     case "confirm":
       replyFormat = confirm(messages)
       break;
     case "thumbnailImage":
-      replyFormat = thumbnailImage(messages)
+      replyFormat = {
+        "type": "template",
+        "altText": "this is a buttons template",
+        "template": {
+          "type": "buttons",
+          "thumbnailImageUrl": message.thumbnailImageUrl,
+          "title": message.title,
+          "text": message.text,
+          "actions": message.actions
+        }
+      }
       break;
     case "carousel":
-      replyFormat = carouselTemplate()
+      replyFormat = carouselTemplate(messages)
       break;
     default:
       replyFormat = { type: 'text', text: type };
