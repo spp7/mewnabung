1a0a9603e87cdcc802bf0e971a1da06ffc3980be
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-08-25 13:28:09 +0700

    carousel edit

--- index.js
+++ index.js
@@ -32,56 +32,50 @@ function handleEvent(event) {
 
   else if (event.message.text === 'Menu'){
     var messagesCarousel = [
-      {
-        "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-        "title": "Games",
-        "text": "Mainkan games seru, dan dapatkan poin!",
-        "actions": [
-          {
-            "type": "postback",
-            "label": "Hangman",
-            "text": "Hangman",
-            "data": "Hangman"
-          },
-          {
-            "type": "postback",
-            "label": "Quiz BCA",
-            "text": "Quiz BCA",
-            "data": "Quiz BCA"
-          }
-        ]
-      },
-      {
-        "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-        "title": "Akun Kamu",
-        "text": "Lihat detail akunmu",
-        "actions": [
-          {
-            "type": "postback",
-            "label": "Cek Saldo",
-            "text": "Cek Saldo",
-            "data": "Cek Saldo"
-          },
-          {
-            "type": "postback",
-            "label": "Alokasi Dana",
-            "text": "Alokasi Dana",
-            "data": "Alokasi Dana"
-          },
-          {
-            "type": "postback",
-            "label": "Set Goals",
-            "text": "Set Goals",
-            "data": "Set Goals"
-          },
-          {
-            "type": "postback",
-            "label": "Cek Poin",
-            "text": "Cek Poin",
-            "data": "Cek Poin"
-          }
-        ]
-      }
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+          "title": "this is menu",
+          "text": "description",
+          "actions": [
+              {
+                  "type": "message",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=111"
+              },
+              {
+                  "type": "message",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=111"
+              },
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/111"
+              }
+          ]
+        },
+        {
+          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+          "title": "this is menu",
+          "text": "description",
+          "actions": [
+              {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=222"
+              },
+              {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=222"
+              },
+              {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/222"
+              }
+          ]
+        }
     ]
     formatReply = line_template.replyMessageFormat("carousel", messagesCarousel)
   }
