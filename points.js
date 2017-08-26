c4832f5347ed27c51a23bc1c29b9fd68f8fff510
renaissains <26372543+renaissains@users.noreply.github.com>
2017-08-26 22:00:29 +0700

    a little change

--- points.js
+++ points.js
@@ -6,7 +6,7 @@ const line_template = require('./line-template')
 const points = function(client, event, unprocessedMessage) {
   let formatReply
   if (unprocessedMessage.stepIdx === pointsActions.steps.length) {
-    formatReply = line_template.replyMessageFormat(null, 'Poinmu untuk saat ini adalah 0. Hahaha')
+    formatReply = line_template.replyMessageFormat(null, `Hi ${unprocessedMessage.result[0]}, poinmu untuk saat ini adalah 0. Hahaha')
   }
   else {
     formatReply = line_template.replyMessageFormat(null, pointsActions.steps[unprocessedMessage.stepIdx].msg)
